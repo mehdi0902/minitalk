8ce9ebf1311516f8946ab0f11a75904418d73f68
mehdi0902 <147936883+mehdi0902@users.noreply.github.com>
2024-05-07 17:45:56 +0200

    oue

---
+++ server.c
@@ -0,0 +1,31 @@
+#include "ft_printf/ft_printf.h"
+
+void	bit_handler(int bit)
+{
+	int	i;
+
+	i = 0;
+	g_msg.c += ((bit & 1) << g_msg.i);
+	g_msg.i++;
+	if (g_msg.i == 7)
+	{
+		ft_printf("%c", g_msg.c);
+		if (!g_msg.c)
+			ft_printf("\n");
+		g_msg.c = 0;
+		g_msg.i = 0;
+	}
+}
+
+int	main(void)
+{
+	ft_printf("marhaba chez moi\n");
+	ft_printf("le PID du serv c'est %d\n", getpid());
+	while (1)
+	{
+		signal(SIGUSR2, bit_handler);
+		signal(SIGUSR1, bit_handler);
+		pause();
+	}
+	return (0);
+}
